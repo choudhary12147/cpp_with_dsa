857e767ecc72111e45c059b42486f0919c45ac71
sunil dhayal <sunildhayal368@gmail.com>
2024-09-03 22:00:41 +0530

    do some more the dynamic array and vector

--- day_15.cpp
+++ day_15.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
 
 // int main(){
@@ -90,38 +90,169 @@ using namespace std;
          2D dynamic array
   */
 
-int main(){
 
-     int row, col;
 
-     cout<<"enter rows: ";
-     cin>>row>>col;
+// int main(){
 
-     int **matrix = new int*[row];
+//      int row, col;
 
-     for (int i = 0; i < row; i++)
-     {
-             matrix[i] = new int[col];
-     }
+//      cout<<"enter rows: ";
+//      cin>>row>>col;
+
+//      int **matrix = new int*[row];
+
+//      for (int i = 0; i < row; i++)
+//      {
+//              matrix[i] = new int[col];
+//      }
       
-       // store the value in the array
+//        // store the value in the array
          
-         int x = 1;
+//          int x = 1;
 
-       for (int i = 0; i <row ; i++)
-       {
-            for (int j = 0; j < col; j++)
-            {
-                matrix[i][j] = x++;
+//        for (int i = 0; i <row ; i++)
+//        {
+//             for (int j = 0; j < col; j++)
+//             {
+//                 matrix[i][j] = x++;
 
-                cout<<matrix[i][j]<<" ";
-            }
+//                 cout<<matrix[i][j]<<" ";
+//             }
             
-            cout<<endl;
-       }
+//             cout<<endl;
+//        }
        
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+/*
+     standard template library of c++
+
+     it is collection of container classes , alogorithms & iterations.
+     it contain implementation of comman datastructure like vectors, stacks,maps etc.
+*/
+
+
+/*
+  vectors like dynamic array they can resize dynamically at runtime . They are stored  contiguously in memory.
+    vectors are also called sequence container
+*/
+
+// intialization of vectors
+
+
+
+
+// int main(){
+     
+//         vector<int> vec1 ;
+
+//         vec1 = {1,2,3,4};
+
+//         cout<<vec1.size()<<"\n";
+
+//         for (int i = 0; i < vec1.size(); i++)
+//         {
+//               cout<<vec1[i]<<" ";
+//         }
+        
+//       // like constrator vector is initailize by -1 value and there is size of 5 
+
+//         vector<int> vec2 (5,-1);
+         
+
+//   return 0;
+// }
+
+
+/*
+   size and capacity of vector
+*/
+
+// int main(){
+         
+//          vector <int> vec;
+           
+//            // size and capacity of vector before push a element 
+
+//            cout<<"size of vec is : "<<vec.size()<<"capacity of vec is : "<<vec.capacity()<<endl;
+           
+
+//           vec.push_back(5);
+
+//           cout<<"size of vec is : "<<vec.size()<<"capacity of vec is : "<<vec.capacity()<<endl;
+
+
+//      return 0;
+// }
+
+
+/*
+    Q. find if any pair in sorted vector has target sum.
+       input : arr[] = {2,7,11,15};
+       output: [0,1]    
+*/
+
+
+// vector<int> pairsum(vector<int> arr, int target){
+         
+//           int st = 0, end = arr.size()-1;
+            
+//             int currsum = 0;
+
+//             vector<int>ans;
+
+//             while (st<end)
+//             {
+//                 currsum = arr[st]+arr[end];
+
+//                 if (currsum == target)
+//                 {
+//                     ans.push_back(st);
+//                     ans.push_back(end);
+//                     return ans;
+
+//                 }else if (currsum<target)
+//                 {
+//                      st++;
+//                 }else
+//                 {
+//                   end--;
+//                 }
+                                                                
+//             }            
+          
+//           return ans;
+
+// }
+
+// int main(){
+
+//        vector<int> vec1 = {2,7,11,15};
+
+//       int target = 9;
+
+//       vector<int> ans = pairsum(vec1,target);
+
+//        for (int i = 0; i < ans.size(); i++)
+//        {
+//           cout<<ans[i]<<" ";
+//        }
+       
+//   return 0;
+// }
+
+
+
+
+/*
+  2D vector 
+*/       
+ 
+
+
 
 
