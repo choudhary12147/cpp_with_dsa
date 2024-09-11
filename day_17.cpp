714705107270bf159345e4fd3ee218f9bd06432c
sunil dhayal <sunildhayal368@gmail.com>
2024-09-11 11:23:13 +0530

    do some topics of oops

--- day_17.cpp
+++ day_17.cpp
@@ -102,17 +102,14 @@ using namespace std;
 
 
 
-class student{
+// class student{
 
      
-      public:
+//       public:
 
-        string name;
-           int age; 
-           int *cgpa;      
-
-
-    
+//         string name;
+//            int age; 
+//            int *cgpa;         
       
     /* non-parameterised constructor */
 
@@ -183,15 +180,209 @@ class student{
 
 
 /*
-      destructor
+      constructor
 */
 
-class 
+// class car{
+    
+//       string name;
 
+//        string color;
+
+//        public:
+//         car(string nameval , string colorval){
+            
+//             name = nameval;
+//             color = colorval;
+
+//            cout<<"constructor is called\n";
+//        }
+     
+//      void start(){
+//         cout<<"car is started";
+//      }
+
+//      void stop(){
+//         cout<<"car is stop";
+//      }
+
+//      //getters 
+
+//      string getname(){
+//         return name;
+//      }
+
+//      string getcolor(){
+//         return color;
+//      }
+
+
+// };
+
+
+
+// int main(){
+         
+//          car c1("maruti","red");
+
+//          cout<<c1.getname()<<endl;
+
+//        return 0;
+// }
+
+
+/*
+    copy constructor :- used to copy properties of one object into another
+*/
+
+// class car{
+
+//    public:
+
+//    string name;
+//    string color;
+       
+//        car(string name , string color){
+         
+//          this->name = name;
+//          this->color = color;
+//        }
+
+// };
+
+
+
+// int main(){
+          
+//           car c1("maruti","white");
+
+//           car c2(c1);
+
+//    // by changing the value of name there is no change in 
+//           c2.name = "alto";
+
+//           cout<<c1.name<<endl;
+//           cout<<c2.name<<endl;
+
+
+//     return 0;
+// }
+
+
+
+/*
+    why we use custom copy constructor
+        
+        because default copy constructor make the shallow copy dynamic allocation 
+
+        if require the deep copy the we use the custom copy constructor
+
+*/
+
+// class car{
+     
+//      public:
+//           string name;
+//           string color;
+//            int *mileage;
+
+
+//        car(string name , string color){
+//            cout<<"constructor is called"<<endl;
+
+//            this->name = name;
+//             this->color = color;
+//             mileage = new int;  // dynamic alloctaion
+//             *mileage = 12;
+//        }
+
+//       car(car &original ){
+//            cout<<"copy original to new \n";
+
+//          name = original.name;
+//          color = original.color;
+//     //    mileage = original.mileage;  // passing the reference that is address of mileage which is already created in heap 
+      
+//       // for deep copy instead of above line we create a new copy in heap using
+
+//              mileage = new int;
+
+//              *mileage = *original.mileage;
+        
+//       }
+
+// };
+
+
+
+// int main(){
+
+//        car c1("maruti","black");
+
+//          car  c2(c1);
+
+//           cout<<"before change in value of mileage "<<endl;   
+
+//          cout<<*c2.mileage<<endl;
+//          cout<<*c1.mileage<<endl;
+
+
+//          *c2.mileage = 20;
+         
+//          cout<<"after change in value of mileage in one obj"<<endl;
+         
+//          cout<<*c2.mileage<<endl;
+//          cout<<*c1.mileage<<endl;
+
+//     return 0;
+// }
+
+
+/*
+   destructor -> deallocate memory from heap  
+*/
+
+class car{
+ 
+   public:
+      
+      string name;
+      string color;
+      int *mileage;
+
+        car(string name , string color ){
+            
+             this->name = name;
+             this->color = color;
+
+             mileage = new int;
+               
+               *mileage = 20;
+
+
+        }
+     
+     ~car(){
+           cout<<"delete the heep memory alloction\n";
+             
+             if (mileage != NULL)
+             {
+                 delete mileage;
+                 mileage = NULL;  
+             }
+             
+     }
+ 
+
+
+};
 
 
 int main(){
 
+    car c1("alto","black");
+       
+       cout<<c1.name<<"\n"<<c1.color<<"\n"<<*c1.mileage<<endl;
 
-       return 0;
+    return 0;
 }
