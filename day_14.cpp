29827b892f27f328e879bec8b209210f48b06f16
sunil dhayal <sunildhayal368@gmail.com>
2024-09-15 22:42:34 +0530

    upercase the string array

--- day_14.cpp
+++ day_14.cpp
@@ -5,52 +5,89 @@ using namespace std;
 
 /*  char  array */
 
-int main(){
+// int main(){
 
-    // char arr[] = {'a' , 'b' , 'c' ,'d','e' '\0'};
+//     // char arr[] = {'a' , 'b' , 'c' ,'d','e' '\0'};
 
-    //   int n = sizeof(arr)/sizeof(arr[0]);
+//     //   int n = sizeof(arr)/sizeof(arr[0]);
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //      cout<<arr[i]<<" ";
-    // }
+//     // for (int i = 0; i < n; i++)
+//     // {
+//     //      cout<<arr[i]<<" ";
+//     // }
 
-    // cout<<arr;
+//     // cout<<arr;
 
-    /*
-        string literals in char arr
-    */
+//     /*
+//         string literals in char arr
+//     */
     
 
-       char name[] = "sunil";
+//        char name[] = "sunil";
 
-       int m = sizeof(name)/sizeof(name[0]);
+//        int m = sizeof(name)/sizeof(name[0]);
 
-       int n = strlen(name);
-       cout<<n<<endl;
-       cout<<m<<endl;
+//        int n = strlen(name);
+//        cout<<n<<endl;
+//        cout<<m<<endl;
 
-       cout<<name<<endl;
+//        cout<<name<<endl;
 
-       cout<<name[3]<<endl;
+//        cout<<name[3]<<endl;
 
-      /*
-         input char array
-      */
+//       /*
+//          input char array
+//       */
 
 
-      //  char word[30];
+//       //  char word[30];
 
-      //  cin>>word;   // ignore the words after the white space
+//       //  cin>>word;   // ignore the words after the white space
 
-      //  cout<<word<<endl;
+//       //  cout<<word<<endl;
 
-       char sentence[30];
+//        char sentence[30];
 
-       cin.getline(sentence,30);
-       cout<<sentence<<endl;
+//        cin.getline(sentence,30);
+//        cout<<sentence<<endl;
 
+
+//     return 0;
+// }
+
+
+/*
+     convert to uppercase
+*/
+
+
+void toupper(char word[] , int n){
+         
+         for (int i = 0; i < n; i++)
+         {
+             if (ch >= 'A' && ch<= 'Z')
+             {
+                  continue;
+             }else
+             {
+                word[i] = ch - 'a' +'A';
+
+             }
+             
+             
+         }
+         
+}
+
+
+
+int main(){
+
+     char word[] = "ApPle";
+
+     toupper(word,strlen(word));
+
+      cout<<word<<endl;
 
     return 0;
 }
