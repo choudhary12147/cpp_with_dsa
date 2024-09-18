ac43eb6c9e2e4b63211ff3dd3afb211a2df18751
sunil dhayal <sunildhayal368@gmail.com>
2024-09-18 22:07:42 +0530

    recursion

---
+++ day_19.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+using namespace std;
+
+
+/*
+       recursion
+*/
+
+
+
+
+// void func(int n){
+      
+//            if (n == 0)
+//            {
+//               return ;
+//            }
+           
+
+//       cout<<"function call..work"<<endl;
+      
+         
+
+//       func(n-1);
+// }
+
+// int main(){
+
+//     func(5);
+
+//     return 0;
+// }
+
+
+
+
+
+/*
+    print number in decreasing order
+*/
+
+
+
+
+// void decreasing(int n){
+    
+//     if (n == 0)
+//     {
+//         return;
+//     }
+          
+//     //   cout<<n<<" ";  // for decreasing order before resursion func call
+    
+//     decreasing(n-1);
+
+//     cout<<n<<" ";     // for increasing order after the recursion func call 
+
+// }
+
+// int main(){
+      
+//       decreasing(5);
+// }
+
+
+
