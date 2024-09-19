88b9efa560ed67800618bf40347f37411d072511
sunil dhayal <sunildhayal368@gmail.com>
2024-09-19 20:01:29 +0530

    recursion

--- day_19.cpp
+++ day_19.cpp
@@ -65,3 +65,119 @@ using namespace std;
 
 
 
+/*
+   sum of n natural number using recursion
+*/
+
+
+
+// int func(int n){
+
+//     int sum;
+
+//     if (n == 0)
+//     {
+//         return sum;
+//     }
+    
+
+//         sum = sum+n;
+   
+//         func(n-1);
+
+// }
+
+// int func(int n){
+       
+//        if (n == 1)
+//        {
+//          return 1;
+//        }
+
+//        return n + func(n-1);
+       
+// }
+
+
+// int main(){
+
+//     int n = 1;
+      
+//      cout<<"sum of n is "<<func(n)<<endl;
+
+//     return 0;
+// }
+
+
+
+
+/*
+
+   fibonacci using recursion
+
+*/
+
+
+// int fibo(int n){
+
+    
+//    if (n==0 || n == 1)
+//    {
+//       return n;
+//    }
+   
+
+//     return fibo(n-1)+fibo(n-2);
+// }
+
+
+
+// int main(){
+
+//    cout<<fibo(10);
+
+//     return 0;
+// }
+
+
+/*
+   check array sorted or not
+*/
+
+
+
+bool isSorted(int arr[] , int n , int i){
+
+          if (i == n-1)
+          {
+              return true;
+          }
+          
+
+             if (arr[i] > arr[i+1])
+             {
+                 return false;
+             }
+             
+          
+          isSorted( arr, n , i+1);
+
+}
+
+
+
+int main(){
+      
+      int arr[] = {1,2,4,5};
+
+      int n = sizeof(arr)/sizeof(arr[0]);
+
+     if (isSorted(arr,n,1))
+     {
+         cout<<"array sorted";
+     }else
+        cout<<"un sorted";
+      
+
+    return 0;
+}
