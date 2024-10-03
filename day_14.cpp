c541fd30785c173198d17de5e8b3f18769fb7c5c
sunil dhayal <sunildhayal368@gmail.com>
2024-10-03 19:07:04 +0530

    revise the concept of string

--- day_14.cpp
+++ day_14.cpp
@@ -204,25 +204,193 @@ using namespace std;
    string input nd output 
 */
 
-int main(){
+// int main(){
+
+
+// /*
+//      input
+// */
+//       string str;
+      
+//       getline(cin,str);
+
+//       cout<<str;
+
+//       /* output for every charcter sepreatly*/
+
+//      //  for (int i = 0; i < str.length(); i++)
+//      //  {
+//      //        cout<<str[i]<<"-";
+//      //  }
+
+//      /* another way of output */
+
+//      for (char ch:str)
+//      {
+//           cout<<ch<<" ";
+//      }
+
+//      cout<<endl;
+     
+//      return 0;
+// }
 
 
 /*
-     input
+     string member function
+
 */
-      string str;
+
+// int main(){
+        
+//         string str = "iamsunildhayal";
+//            cout<<str.length();
+//           cout<<str.size(); //    
+//         cout<<str.at(5)<<endl;
+//         cout<<str.find("dh")<<endl;  /* it return -1 if am not persent in str like int idx = str.find("am")*/
+//         cout<<str.substr(2,5);
+
+
+
+//      return 0;
+// }
+
+
+/*
+    anagram
+*/
+
+// bool isanagram(string str1 , string str2){
+          
+//           int count[26];
+
+//           if (str1.length() !=  str2.length())
+//           {
+//                  cout<<"not an anagram"<<endl;
+//                  return false;
+//           }
+          
+
+//           for (int i = 0; i < str1.length(); i++)
+//           {
+//                  int idx = str1[i] - 'a';
+                  
+//                   count[idx]++;
+
+//           }
+
+//           for (int i = 0; i < str2.length(); i++)
+//           {
+                
+//                  int idx = str2[i] - 'a';
+
+//                  if (str2[idx] == 0)
+//                  {
+//                     cout<<"not a valid anagram"<<endl;
+//                       return false;
+//                  }
+
+//                  count[idx]--;
+                 
+//           }
+          
+//            return true;
+
+// }
+
+
+// int main(){
       
-      getline(cin,str);
+//       string str1 = "anagram";
+//       string str2 = "ngaram";
 
-      cout<<str;
+//        if(isanagram(str1,str2)){
+//           cout<<"anagram"<<endl;
+//        }else
+//        {
+//           cout<<"not an anagram"<<endl;
+//        }
+       
 
-      /* output for every charcter sepreatly*/
+//      return 0;
+// }
 
-      for (int i = 0; i < str.length(); i++)
-      {
-            cout<<str[i]<<"-";
-      }
+
+/*
+       Question 1 : Count how many times lowercase vowels occurred in a String entered 
+                    by the user.
+*/
+
+// int getVowels(string str){
       
+//           int count = 0;
 
-     return 0;
-}
+//           for (int i = 0; i < str.length(); i++)
+//           {
+//              if (str[i] == 'a' || str[i] == 'e' ||
+//                str[i] = 'i' || str[i] == 'o'||
+//                str[i] == 'u')
+//                {
+//                     count++;
+//                }
+               
+//           }
+       
+//       //  cout<<count<<endl;
+
+//           return count;
+          
+// }
+
+
+// int main(){
+
+//         string str = "SunilDHAyal";
+
+//        getVowels(str);
+        
+
+//    return 0;
+// }
+
+/*
+         Question 2 : You are given two strings s1 and s2 of equal length. A string swap is an 
+                  operation where you choose two indices in a string (not necessarily different) and 
+                  swap the characters at these indices.
+                  Return true if it is possible to make both strings equal by performing at most one 
+                 string swap on exactly one of the strings. Otherwise, return false.
+                   Example :
+                       Input: s1 = "bank", s2 = "kanb" Output: 
+                           true
+                         Explanation: For example, swap the first character with the last character of s2 to 
+                              make "ban
+*/
+
+// void isEqual(string str1, string str2){
+         
+//            int st = 0;
+//            int end = str2.length()-1;
+
+//            char temp = str2[st];
+                   
+//              str2[st] = str2[end];
+//              str2[end] = temp;
+
+//              if (str1 ==  str2)
+//              {
+//                cout<<"equal";
+//              }
+             
+// }
+
+
+// int main(){
+
+//       string str1 = "bank";
+
+//       string str2 = "lanb";
+
+//       isEqual(str1,str2);
+
+//    return 0;
+// }
