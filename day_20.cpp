5417b0106e72d1f65f3b944be57adc67f3f97543
sunil dhayal <sunildhayal368@gmail.com>
2024-10-06 23:51:58 +0530

    do some divide and conquer problem

--- day_20.cpp
+++ day_20.cpp
@@ -5,20 +5,42 @@ using namespace std;
 // tiling problem
 
 
-int tillingprob(int n){
+// int tillingprob(int n){
 
-    if (n == 0 || n == 1)
-    {
-          return 1;
-    }
+//     if (n == 0 || n == 1)
+//     {
+//           return 1;
+//     }
 
-    return tillingprob(n-1) + tillingprob(n-2);
+//     return tillingprob(n-1) + tillingprob(n-2);
 
-}
+// }
+
+
+
+//  remove duplicates in string
+
+// void romove( string str , int i){
+
+//        if (i == str.size())
+//        {
+//         /* code */
+//        }
+       
+// }
+
+
+
+
 
 int main(){
 
-    cout<<tillingprob(5);
+    // cout<<tillingprob(5);
+
+
+    string name = "sunil";
+
+    cout<<name.size()<<endl;
 
     return 0;
 }
