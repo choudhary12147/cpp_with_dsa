948d893ec74ae433a65852237ab6d76d503d781f
sunil dhayal <sunildhayal368@gmail.com>
2025-02-04 23:21:27 +0530

    dll

--- day_26.cpp
+++ day_26.cpp
@@ -1,28 +1,83 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    
+     Node *prev;
+     int data;
+     Node *next;
+
+   public:
+
+     Node(int val){
+        this->data = val;
+        this->prev = nullptr;
+        this->next = nullptr;
+     }
+
+}
+
+class doublylist{
+  
+  public:
+
+   Node*head;
+   Node*tail;
+
+   doublelist(){
+      head tail = nullptr;
+   }
+
+  void push_front(int val){
+       Node *newnode = new Node(val);
+
+       if (head == nullptr)
+       {
+              head = newnode;
+       }else{
+            
+            newnode->next = head;
+            head->prev = newnode;
+            head = newnode;
+       }
+       
+  }
+
+}
+
+
+
 int main(){
 
-    list <int> ll;
+  //   list <int> ll;
 
-    ll.push_front(1);
-    ll.push_back(2);
+  //   ll.push_front(1);
+  //   ll.push_back(2);
      
-    for(auto it : ll){
+  //   for(auto it : ll){
 
-        cout<<it<<endl;
-    }
+  //       cout<<it<<endl;
+  //   }
 
- 
    
-   ll.insert(ll.begin(),5);
+  //  ll.insert(ll.begin(),5);
 
-     list<int> :: iterator itr;
+  //    list<int> :: iterator itr;
 
-   for (itr = ll.begin();itr != ll.end();itr++)
-   {
-          cout<<(*itr)<<" -> ";
-   }
+  //  for (itr = ll.begin();itr != ll.end();itr++)
+  //  {
+  //         cout<<(*itr)<<" -> ";
+  //  }
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
